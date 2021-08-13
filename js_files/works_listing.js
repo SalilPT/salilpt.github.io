bebc9ccd71c9f5a16863c5e4aa18fbc08739c5ac
SalilPT <32131426+SalilPT@users.noreply.github.com>
2021-08-13 01:07:05 -0700

    Programmed the buttons that filter by type

--- js_files/works_listing.js
+++ js_files/works_listing.js
@@ -1,1 +1,63 @@
+/**************************************************************************************************
+BUTTONS FOR FILTERING BY TYPE
+**************************************************************************************************/
 
+/*
+Function to filter elements by tag. Elements filtered out are hidden from the
+document using the document's CSS file.
+
+Parameters:
+CSS_elem_selector: A string that is a valid CSS selector for the elements to filter.
+target_tags: An array of strings, in which each string is a tag. When a
+filtered element contains any one of the tags, it passes through the filter.
+*/
+function filterByTag(CSS_elem_selector, target_tags) {
+
+	var entries = document.querySelectorAll(CSS_elem_selector);
+
+	for (var i = 0; i < entries.length; i++) {
+		/*Get the tags of the current HTML element as an array of strings. In the
+		HTML document, the tags are delimited by commas.*/
+		var tag_array = entries[i].getAttribute("data-listing-tags").toString().split(",");
+
+		/* Flag for when the current element passes through filtering by type*/
+		var tag_match = false;
+
+		/*Loop through all tags of the current element*/
+		for (var tag_idx = 0; tag_idx < target_tags.length; tag_idx++) {
+			if (tag_array.includes(target_tags[tag_idx])) {
+				tag_match = true;
+				break;
+			}
+		}
+
+		/*Set visibility of current element based on whether or not at least one of
+		the element's tags exist in the target tags*/
+		if (tag_match) {
+			entries[i].classList.remove("hidden");
+		}
+		else {
+			entries[i].classList.add("hidden");
+		}
+		tag_match = false;
+	}
+}
+
+/*Use event delegation on the section buttons container to filter the entries when a button is clicked*/
+document.getElementById("section_buttons_container").onclick = function(e) {
+	if (e.target.id != "all_entries_button" && e.target.className == "section_button") {
+		/*Note: data-type-filter may have more than one word for the filter, separated by a comma*/
+		filterByTag("[data-listing-tags]", e.target.getAttribute("data-type-filter").toString().split(","));
+	}
+
+	/*If the all entries button was clicked, make it so no entries are hidden*/
+	else if (e.target.id == "all_entries_button") {
+		var entries=document.querySelectorAll("[data-listing-tags]");
+		for (e_idx = 0; e_idx < entries.length; e_idx++) {
+			entries[e_idx].classList.remove("hidden");
+		}
+	}
+}
+
+/**************************************************************************************************
+**************************************************************************************************/
